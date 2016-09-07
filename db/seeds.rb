affe725a8341f9e5c1c9a17eacabdc8b2d64137a
billhimmelsbach <whimmels@gmail.com>
2016-09-07 01:25:51 -0700

    makes small changes to layout, adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+Switt.destroy_all
+
+p "Destroyed all the Switts"
+
+SWITTS = [
+  {
+    name: "Turbo Switt",
+    super_power: "Faster than the normal Switt"
+  },
+  {
+    name: "CEO Switt",
+    super_power: "Able to delegate tasks, looks good in a button up"
+  }
+]
+
+p Switt.create(SWITTS)
+s = Switt.all
+puts "Seeded #{s.count} switts."
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
